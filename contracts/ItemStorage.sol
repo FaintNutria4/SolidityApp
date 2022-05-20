39dae223d2a638cc8e3321812e5dca2ff98027d0
Álvaro <adepablos@edu.tecnocampus.cat>
2022-05-20 19:51:41 +0200

    item store update
    New stuff:
    - struct item
    - itemStats map
    - id2Balance map
    - address2Balances map
    - createItem function
    - getBalances function

--- contracts/ItemStorage.sol
+++ contracts/ItemStorage.sol
@@ -3,25 +3,29 @@ pragma solidity 0.8.11;
 
 contract ItemStorage {
 
-      mapping (uint => uint) id2Balance;
-      mapping (address = > id2balance) address2Balances;
+   mapping (uint => uint) id2Balance;
+   mapping (address = > id2balance) address2Balances;
 
-    struct Item {
+   mapping(uint => Item) itemStats;
+
+   struct Item {
       uint idType;
       string name;
       string description;
       uint damage;
-    }
+   }
 
-    constructor() {
+   function createItem(uint _idType, string _name, string _description, uint damage, address _owner) public returns (Item) {
+      Item newItem = Item(_idType, _name, _description, damage);
+      itemStats[_idType] = newItem;
+      address2Balances[_owner][_idType] = address2Balances[_owner][_idType] + 1;
 
-    }
+      return newItem;
 
-    function  getId() public view returns (uint) {
-       return id;
-    }
+   }
 
-    function  getDmg() public view returns (uint) {
-       return dmg;
-    }
-}
+   function getBalances() public returns (id2balance){
+      return address2Balances[msg.sender];
+   }
+
+ }
